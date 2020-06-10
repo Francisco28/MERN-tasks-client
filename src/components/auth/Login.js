c56b39d2f939d83ee73fce3600b66845bc7a6835
francisco28 <fgallegosgo@gmail.com>
2020-06-10 13:07:28 -0600

    creating the Login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,8 +2,50 @@ import React from 'react';
 
 
 const Login = () => {
+
+    //function that will execute every the user write
+    const onChange = () => {
+
+    }
+
     return ( 
-        <h1>From Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Log in</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Log in"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
