6340ef7d1384d68fa6dd130b818f320a87453207
francisco28 <fgallegosgo@gmail.com>
2020-06-10 16:16:48 -0600

    creating state and functions for news projects

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,8 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 
 
 const NewProject = () => {
+
+
+    //State for Project
+    const [ project, saveProject ] = useState({
+        name: ''
+    });
+
+    //extract name of project
+    const { name } = project;
+
+    //read the contents of input
+    const onChangeProject = e => {
+        saveProject({
+            ...project,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    //when the user press submit input
+    const onSubmitProject = e => {
+        e.preventDefault();
+        
+        //validate the project
+
+        //add to state
+
+        //restart the form
+
+    }
+
   return (
     <Fragment>
         <button 
@@ -12,12 +42,15 @@ const NewProject = () => {
 
         <form
             className="formulario-nuevo-proyecto"
+            onSubmit={onSubmitProject}
         >
                 <input 
                     type="text"
                     className="input-text"
                     placeholder="Name of Project"
                     name="name"
+                    value={name}
+                    onChange={onChangeProject}
                 />
 
                 <input 
