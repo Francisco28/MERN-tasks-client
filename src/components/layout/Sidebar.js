12bd71e97d57004624fb152eb8bebfefda6c8987
francisco28 <fgallegosgo@gmail.com>
2020-06-10 15:55:19 -0600

    creating the form for news projects

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import NewProject from '../projects/NewProject';
 
 const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN<span>Tasks</span></h1>
+
+            <NewProject />
 
             <div className="proyectos">
                 <h2>Your Projects</h2>
---
+++ src/components/projects/NewProject.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+
+
+
+const NewProject = () => {
+  return (
+    <Fragment>
+        <button 
+            type="button" 
+            className="btn btn-block btn-primario"
+        >New Project</button>
+
+        <form
+            className="formulario-nuevo-proyecto"
+        >
+                <input 
+                    type="text"
+                    className="input-text"
+                    placeholder="Name of Project"
+                    name="name"
+                />
+
+                <input 
+                    type="submit"
+                    className="btn btn-primario btn-block"
+                    value="Add Project"
+                />
+
+        </form>
+    </Fragment>
+  );
+};
+
+export default NewProject;
