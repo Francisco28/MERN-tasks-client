7fa98a35f95270eeca6d2e8e84389bb2e08b2171
francisco28 <fgallegosgo@gmail.com>
2020-06-10 15:35:36 -0600

    first steps

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import Sidebar from '../layout/Sidebar';
 
 const Projects = () => {
     return ( 
-        <h1>From Projects</h1>
+        
+        <div className="contenedor-app">
+            <Sidebar />
+            
+            <div className="seccion-principal">
+                <main>
+
+                    <div className="contenedor-tareas">
+
+                    </div>
+                </main>
+            </div>
+        </div>
+
      );
 }
  
