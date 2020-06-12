1dad33a2cdba7fa6dd63d5306a28b6e33470ca50
francisco28 <fgallegosgo@gmail.com>
2020-06-12 13:42:01 -0600

    oculting the Task Form if there is not project selected

--- src/components/tasks/FormTask.js
+++ src/components/tasks/FormTask.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import projectContext from '../../context/projects/projectContext';
 
 
 const FormTask = () => {
+
+    //Extract pif a project is active
+    const projectsContext = useContext(projectContext);
+    const { project } = projectsContext;
+
+    //if there is not project selected
+    if(!project) return null;
+    
+    //Array destructuring for extract the current project
+    const [currentProject] = project;
+
+
     return ( 
         <div className="formulario">
             <form>
